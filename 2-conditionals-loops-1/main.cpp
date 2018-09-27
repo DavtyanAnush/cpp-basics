d192b99448a4cb50250bddaa67122bdb529fd82f
Anush <*dawtyan.anush@yandex.ru*>
2018-09-27 22:33:14 +0300

    The second commit

--- 2-conditionals-loops-1/main.cpp
+++ 2-conditionals-loops-1/main.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 int main()
 {
-	double x1, x2, dx, a, b, c,i, F=0,x;
+	double x1, x2, dx, a, b, c, F,x;
 	cout << "Enter a, b, c." << endl;
 	cin >> a >> b >> c;
 	cout << "Enter x1" << endl;	
@@ -12,24 +12,122 @@ int main()
 	cin >> x2;
 	cout << "Enter dx" << endl;
 	cin >> dx;
-	x = x1;
-	while (x < x2)
+	if (dx != 0) 
 	{
+	cout<<"-----------------------------------------"<<endl;
+	cout << "|";
+	cout.width(10);
+	cout <<"x";
+	cout.width(10);
+	cout << "|";
+	cout.width(10);
+	cout << "F";
+	cout.width(10);
+	cout << "|"<<endl;
+	cout << "-----------------------------------------" << endl;
+	x = x1;
+		while (x <x2)
+		{
+			if ((c < 0)&(b != 0)) {
+				F = a * pow(x, 2) + pow(b, 2)*x;
+			}
+			else
+			{
+				if ((c > 0)&(b == 0))
+				{
+					if (x + c != 0)
+						F = (x + a) / (x + c);
+					else
+						cout << "Oshibka!";
+				}
+				else
+					if (c != 0)
+						F = x / c;
+					else
+						cout << "Oshibka!";
+			}
+			if (((int(a)&int(b)) | (int(a)&int(c))) != 0) 
+			{
+				cout << "|";
+				cout.width(10);
+				cout << fixed;
+				cout.precision(3);
+				cout << showpos << x;
+				cout.width(10);
+				cout << "|";
+				cout.width(10);
+				cout << F;
+				cout.width(10);
+				cout << "|" << endl;
+				x += dx;
+			}
+			else 
+			{
+				cout << "|";
+				cout.width(10);
+				cout << fixed;
+				cout.precision(3);
+				cout << showpos << x;
+				cout.width(10);
+				cout << "|";
+				cout.width(10);
+				cout << int(F);
+				cout.width(10);
+				cout << "|" << endl;
+				x += dx;
+			}
+		}
 		if ((c < 0)&(b != 0)) {
-			F = a * pow(x, 2) + pow(b, 2)*x;
+			F = a * pow(x2, 2) + pow(b, 2)*x2;
 		}
 		else
 		{
 			if ((c > 0)&(b == 0))
-		    {
-			F = (x + a) / (x + c);
-		    }
+			{
+				if (x2 + c != 0)
+					F = (x2 + a) / (x2 + c);
+				else
+					cout << "Oshibka!";
+			}
 			else
-			F=x/c;
+				if (c != 0)
+					F = x2 / c;
+				else
+					cout << "Oshibka!";
 		}
-		
-		cout << F << '|';
-		x = x + dx;
+		if (((int(a)&int(b)) | (int(a)&int(c)))!=0) 
+		{
+			cout << "|";
+			cout.width(10);
+			cout << fixed;
+			cout.precision(3);
+			cout << showpos << x2;
+			cout.width(10);
+			cout << "|";
+			cout.width(10);
+			cout << F;
+			cout.width(10);
+			cout << "|" << endl;
+		}
+		else
+		{
+			cout << "|";
+			cout.width(10);
+			cout << fixed;
+			cout.precision(3);
+			cout << showpos << x2;
+			cout.width(10);
+			cout << "|";
+			cout.width(10);
+			cout << int(F);
+			cout.width(10);
+			cout << "|" << endl;
+
+		}
+		cout << "-----------------------------------------" << endl;
 	}
+	
+	else
+		cout << "Oshibka!";
 	return 0;
 }
