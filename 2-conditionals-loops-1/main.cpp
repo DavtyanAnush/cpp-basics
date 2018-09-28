0f5a07e16a4a2a8b2362b2684828a004d39efcec
Anush <*dawtyan.anush@yandex.ru*>
2018-09-29 01:27:44 +0300

    The third commit

--- 2-conditionals-loops-1/main.cpp
+++ 2-conditionals-loops-1/main.cpp
@@ -3,46 +3,38 @@
 using namespace std;
 int main()
 {
-	double x1, x2, dx, a, b, c, F,x;
-<<<<<<< HEAD
+	double x1, x2, dx, a, b, c, F, x;
 	const long double EXP = 0.000000000001;
-=======
->>>>>>> d192b99448a4cb50250bddaa67122bdb529fd82f
 	cout << "Enter a, b, c." << endl;
 	cin >> a >> b >> c;
-	cout << "Enter x1" << endl;	
+	cout << "Enter x1" << endl;
 	cin >> x1;
 	cout << "Enter x2" << endl;
 	cin >> x2;
 	cout << "Enter dx" << endl;
 	cin >> dx;
-	if (dx != 0) 
+	if (dx != 0)
 	{
-<<<<<<< HEAD
-	cout << "-----------------------------------------" << endl;
-=======
-	cout<<"-----------------------------------------"<<endl;
->>>>>>> d192b99448a4cb50250bddaa67122bdb529fd82f
-	cout << "|";
-	cout.width(10);
-	cout <<"x";
-	cout.width(10);
-	cout << "|";
-	cout.width(10);
-	cout << "F";
-	cout.width(10);
-	cout << "|"<<endl;
-	cout << "-----------------------------------------" << endl;
-	x = x1;
-<<<<<<< HEAD
-		while (x <=x2)
+		cout << "-----------------------------------------" << endl;
+		cout << "|";
+		cout.width(10);
+		cout << "x";
+		cout.width(10);
+		cout << "|";
+		cout.width(10);
+		cout << "F";
+		cout.width(10);
+		cout << "|" << endl;
+		cout << "-----------------------------------------" << endl;
+		x = x1;
+		while (x <= x2)
 		{
-			if ((c < 0)&(abs(b)>EXP)){
+			if ((c < 0)&(abs(b) > EXP)) {
 				F = a * pow(x, 2) + pow(b, 2)*x;
 			}
 			else
 			{
-				if ((c > 0)&(abs(b)<EXP))
+				if ((c > 0)&(abs(b) < EXP))
 				{
 					if (x + c != 0)
 						F = (x + a) / (x + c);
@@ -55,7 +47,7 @@ int main()
 					else
 						cout << "Oshibka!";
 			}
-			if (((int(a)&int(b)) | (int(a)&int(c))) != 0) 
+			if (((int(a)&int(b)) | (int(a)&int(c))) != 0)
 			{
 				cout << "|";
 				cout.width(10);
@@ -69,7 +61,7 @@ int main()
 				cout.width(10);
 				cout << "|" << endl;
 			}
-			else 
+			else
 			{
 				cout << "|";
 				cout.width(10);
@@ -87,109 +79,6 @@ int main()
 		}
 		cout << "-----------------------------------------" << endl;
 	}
-=======
-		while (x <x2)
-		{
-			if ((c < 0)&(b != 0)) {
-				F = a * pow(x, 2) + pow(b, 2)*x;
-			}
-			else
-			{
-				if ((c > 0)&(b == 0))
-				{
-					if (x + c != 0)
-						F = (x + a) / (x + c);
-					else
-						cout << "Oshibka!";
-				}
-				else
-					if (c != 0)
-						F = x / c;
-					else
-						cout << "Oshibka!";
-			}
-			if (((int(a)&int(b)) | (int(a)&int(c))) != 0) 
-			{
-				cout << "|";
-				cout.width(10);
-				cout << fixed;
-				cout.precision(3);
-				cout << showpos << x;
-				cout.width(10);
-				cout << "|";
-				cout.width(10);
-				cout << F;
-				cout.width(10);
-				cout << "|" << endl;
-				x += dx;
-			}
-			else 
-			{
-				cout << "|";
-				cout.width(10);
-				cout << fixed;
-				cout.precision(3);
-				cout << showpos << x;
-				cout.width(10);
-				cout << "|";
-				cout.width(10);
-				cout << int(F);
-				cout.width(10);
-				cout << "|" << endl;
-				x += dx;
-			}
-		}
-		if ((c < 0)&(b != 0)) {
-			F = a * pow(x2, 2) + pow(b, 2)*x2;
-		}
-		else
-		{
-			if ((c > 0)&(b == 0))
-			{
-				if (x2 + c != 0)
-					F = (x2 + a) / (x2 + c);
-				else
-					cout << "Oshibka!";
-			}
-			else
-				if (c != 0)
-					F = x2 / c;
-				else
-					cout << "Oshibka!";
-		}
-		if (((int(a)&int(b)) | (int(a)&int(c)))!=0) 
-		{
-			cout << "|";
-			cout.width(10);
-			cout << fixed;
-			cout.precision(3);
-			cout << showpos << x2;
-			cout.width(10);
-			cout << "|";
-			cout.width(10);
-			cout << F;
-			cout.width(10);
-			cout << "|" << endl;
-		}
-		else
-		{
-			cout << "|";
-			cout.width(10);
-			cout << fixed;
-			cout.precision(3);
-			cout << showpos << x2;
-			cout.width(10);
-			cout << "|";
-			cout.width(10);
-			cout << int(F);
-			cout.width(10);
-			cout << "|" << endl;
-
-		}
-		cout << "-----------------------------------------" << endl;
-	}
-	
->>>>>>> d192b99448a4cb50250bddaa67122bdb529fd82f
 	else
 		cout << "Oshibka!";
 	return 0;
