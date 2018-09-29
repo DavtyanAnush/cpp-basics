675cfb2b2cc083f94c3623a4bdfa42d29fab066d
Anush <*dawtyan.anush@yandex.ru*>
2018-09-29 16:00:57 +0300

    The fourth commit

--- 2-conditionals-loops-1/main.cpp
+++ 2-conditionals-loops-1/main.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
+#include <string>
 #include <cmath>
 using namespace std;
 int main()
 {
 	double x1, x2, dx, a, b, c, F, x;
-	const long double EXP = 0.000000000001;
+	const double EPS = DBL_EPSILON;
 	cout << "Enter a, b, c." << endl;
 	cin >> a >> b >> c;
 	cout << "Enter x1" << endl;
@@ -13,9 +14,9 @@ int main()
 	cin >> x2;
 	cout << "Enter dx" << endl;
 	cin >> dx;
-	if (dx != 0)
+	if (abs(dx) >EPS)
 	{
-		cout << "-----------------------------------------" << endl;
+		cout << string(41, '-') << endl;
 		cout << "|";
 		cout.width(10);
 		cout << "x";
@@ -25,59 +26,45 @@ int main()
 		cout << "F";
 		cout.width(10);
 		cout << "|" << endl;
-		cout << "-----------------------------------------" << endl;
+		cout << string(41, '-') << endl;
 		x = x1;
 		while (x <= x2)
 		{
-			if ((c < 0)&(abs(b) > EXP)) {
+			if ((c < 0)&(abs(b) > EPS)) {
 				F = a * pow(x, 2) + pow(b, 2)*x;
 			}
 			else
 			{
-				if ((c > 0)&(abs(b) < EXP))
+				if ((c > 0)&(abs(b) < EPS))
 				{
-					if (x + c != 0)
+					if (abs(x + c) >EPS)
 						F = (x + a) / (x + c);
 					else
-						cout << "Oshibka!";
+						cout << "Oshibka deleniya na '0'!";
 				}
 				else
-					if (c != 0)
+					if (abs(c) >EPS)
 						F = x / c;
 					else
-						cout << "Oshibka!";
-			}
+						cout << "Oshibka deleniya na '0'!";
+			} 
+			cout << "|";
+			cout.width(10);
+            cout << fixed;
+			cout.precision(3);
+			cout << x;
+			cout.width(10);
+			cout << "|";
+			cout.width(10);
 			if (((int(a)&int(b)) | (int(a)&int(c))) != 0)
-			{
-				cout << "|";
-				cout.width(10);
-				cout << fixed;
-				cout.precision(3);
-				cout << showpos << x;
-				cout.width(10);
-				cout << "|";
-				cout.width(10);
 				cout << F;
-				cout.width(10);
-				cout << "|" << endl;
-			}
 			else
-			{
-				cout << "|";
-				cout.width(10);
-				cout << fixed;
-				cout.precision(3);
-				cout << showpos << x;
-				cout.width(10);
-				cout << "|";
-				cout.width(10);
 				cout << int(F);
-				cout.width(10);
-				cout << "|" << endl;
-			}
+			cout.width(10);
+			cout << "|" << endl;
 			x += dx;
 		}
-		cout << "-----------------------------------------" << endl;
+		cout << string(41, '-') << endl;
 	}
 	else
 		cout << "Oshibka!";
