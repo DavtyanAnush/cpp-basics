557cf9586e938b56483832867c60606aa773e228
Anush <*dawtyan.anush@yandex.ru*>
2018-09-30 12:40:00 +0300

    1commit

--- 2-conditionals-loops-1/main.cpp
+++ 2-conditionals-loops-1/main.cpp
@@ -1,78 +1,78 @@
 #include <iostream>
 #include <string>
 #include <cmath>
+#include <iomanip>
+
 using namespace std;
+
 int main()
 {
-	double x1, x2, dx, a, b, c, F, x;
-	const double EPS = DBL_EPSILON;
-	cout << "Enter a, b, c." << endl;
-	cin >> a >> b >> c;
-	cout << "Enter x1" << endl;
+	double x1, x2, dx, a, b, c, f, x;
+	const double EPS = 1e-15;
+
+	cout << "Enter a: ";
+	cin >> a;
+	cout << "Enter b: ";
+	cin >> b;
+	cout << "Enter c: ";
+	cin >> c;
+	cout << "Enter x1: ";
 	cin >> x1;
-	cout << "Enter x2" << endl;
+	cout << "Enter x2: ";
 	cin >> x2;
-	cout << "Enter dx" << endl;
+	cout << "Enter dx: ";
 	cin >> dx;
-	if ((abs(dx) >EPS)&(abs(c) > EPS))
+
+	if ((abs(dx) > EPS) && (abs(c) > EPS))
 	{
-		cout << string(41, '-') << endl;
-		cout << "|";
-		cout.width(10);
-		cout << "x";
-		cout.width(10);
-		cout << "|";
-		cout.width(10);
-		cout << "F";
-		cout.width(10);
-		cout << "|" << endl;
-		cout << string(41, '-') << endl;
+		cout << endl << string(37, '-') << endl;
+		cout << "|" << setw(9) << "x" << setw(9);
+		cout << "|" << setw(9) << "f" << setw(9) << "|";
+		cout << endl << string(37, '-') << endl;
+
 		cout << fixed;
 		cout.precision(3);
+
 		x = x1;
 		while (x <= x2)
 		{
-			if ((c < 0)&(abs(b) > EPS)) {
-				F = a * pow(x, 2) + pow(b, 2)*x;
+			if ((c < 0) && (abs(b) > EPS))
+			{
+				f = a * pow(x, 2) + pow(b, 2) * x;
 			}
 			else
 			{
-				if ((c > 0)&(abs(b) < EPS))
+				if ((c > 0) && (abs(b) < EPS))
 				{
-					if (abs(x + c) >EPS)
-						F = (x + a) / (x + c);
-					else {
-						cout << "|";
-						cout.width(10);
-						cout << x;
-						cout.width(10);
-						cout << "|";
-						cout.width(10);
-						cout << "division by zero";
-						cout.width(4);
-						cout << "|" << endl;
+					if (abs(x + c) > EPS)
+						f = (x + a) / (x + c);
+					else
+					{
+						cout << "|" << setw(11) << x << setw(7);
+						cout << "|" << "  division by 0  |\n";
+						x += dx;
+						continue;
 					}
 				}
 				else
-						F = x / c;
-			} 
-			cout << "|";
-			cout.width(10);
-			cout << x;
-			cout.width(10);
-			cout << "|";
-			cout.width(10);
-			if (((int(a)&int(b)) | (int(a)&int(c))) != 0)
-				cout << F;
+					f = x / c;
+			}
+
+			cout << "|" << setw(11) << x << setw(7) << "|";
+
+			if (((int(a) & int(b)) | (int(a) & int(c))) != 0)
+				cout << setw(11) << f << setw(7);
 			else
-				cout << int(F);
-			cout.width(10);
+				cout << setw(9) << int(f) << setw(9);
+
 			cout << "|" << endl;
+
 			x += dx;
 		}
-		cout << string(41, '-') << endl;
+		cout << string(37, '-') << endl;
 	}
 	else
-		cout << "Oshibka!";
+		cout << "\nError!\n";
+
 	return 0;
 }
