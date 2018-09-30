93ce6b60bbac4e1ea6c9aae57f778f141eaee826
Anush <*dawtyan.anush@yandex.ru*>
2018-10-01 02:23:32 +0300

    5commit

--- 3-conditionals-loops-2/main.cpp
+++ 3-conditionals-loops-2/main.cpp
@@ -1,13 +1,16 @@
 #include <iostream>
 #include <string>
 #include <cmath>
+#include <iomanip>
+
 using namespace std;
+
 int main()
 {
-	double x, x1, x2, dx, y,p, eps;
-	int n;
-	const double EPS = DBL_EPSILON;
+	double x, x1, x2, dx, y=0,p, eps;
+	const double EPS = 1e-15;
 	const int n=100;
+
 	check:
 	cout << "In ln(1-x), -1 <= x < 1"<<endl<< "Enter x1, x2, dx, eps" << endl;
 	cin >> x1 >> x2 >> dx >> eps;
@@ -16,56 +19,31 @@ int main()
 		cout << "Input error" << endl;
 		goto check;
 	}
-	cout << string(41, '-') << endl; 
-	cout << "|"; 
-	cout.width(5); 
-	cout << "n"; 
-	cout.width(5); 
-	cout << "|";
-	cout.width(5); 
-	cout << "x";
-	cout.width(5);
-	cout << "|"; 
-	cout.width(5);
-	cout << "ln(1-x)";
-	cout.width(5);
-	cout << "|";
-	cout.width(5);
-	cout << "y";
-	cout.width(5);
-	cout << "|"<<endl;
-	cout << string(41, '-') << endl;
+	cout << string(73, '-') << endl; 
+	cout << "|" << setw(9) << "n" << setw(9);
+	cout << "|" << setw(9) << "x" << setw(9);
+	cout << "|" << setw(9) << "ln(1-x)" << setw(9);
+	cout << "|" << setw(9) << "y" << setw(9)<< "|" << endl;
+	cout << string(73, '-') << endl;
 	x = x1;
-	while (x<x2)
+	while (x<=x2)
 	{
 	  int i = 1;
       while (i<n)
 	  {
 		y +=(pow(x, i) / i);
+		if (abs(y) < eps)
+			break;
 		i++; 
 	  }
-	           p = log(1 - x);
-	y = -1*y;
-	cout << "|";
-	cout.width(5);
-	cout << showpos<< i;
-	cout.width(5);
-	cout << "|";
-	cout.width(5);
-	cout << fixed;
-	cout.precision(eps);
-	cout << showpos << x;
-	cout.width(5);
-	cout << "|"; 
-	cout.width(5);
-	cout << showpos << p;
-	cout.width(5);
-	cout << "|";
-	cout.width(5);
-	cout << showpos << y;
-	cout.width(5);
-	cout << "|"<<endl;
-	x = x + dx;
-	}	
+	  y = -1*y;
+	  p = log(1 - x);
+	  cout << "|" << setw(11) << i << setw(7) << "|";
+	  cout << setw(11) << x << setw(7) << "|";
+	  cout << setw(11) << p << setw(7) << "|";
+	  cout << setw(11) << y << setw(7) << "|"<< endl;
+	  x += dx;
+	}
+	cout << string(73, '-') << endl;
 	return 0;
 }
