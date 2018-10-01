ecc707009416282ba41f7b509dd4a3533f434d40
Anush <*dawtyan.anush@yandex.ru*>
2018-10-01 19:48:24 +0300

    6 commit

--- 3-conditionals-loops-2/main.cpp
+++ 3-conditionals-loops-2/main.cpp
@@ -7,42 +7,53 @@ using namespace std;
 
 int main()
 {
-	double x, x1, x2, dx, y=0,p, eps;
-	const double EPS = 1e-15;
-	const int n=100;
+	double x, x1, x2, dx, y, eps;
+	const double kEPS = 1e-15;
+	const int i_max=100;
 
 	check:
-	cout << "In ln(1-x), -1 <= x < 1"<<endl<< "Enter x1, x2, dx, eps" << endl;
+	cout << "ln(1-x), -1 <= x < 1"<<endl<< "Enter x1, x2, dx, eps" << endl;
 	cin >> x1 >> x2 >> dx >> eps;
-	if (((abs(dx) < EPS)||((x1 >= 1)||(x1 < -1))||((x2 >= 1)||(x2 < -1))))
+	if (((abs(dx) < kEPS)||((x1 >= 1)||(x1 < -1))||((x2 >= 1)||(x2 < -1))))
 	{
 		cout << "Input error" << endl;
 		goto check;
 	}
 	cout << string(73, '-') << endl; 
-	cout << "|" << setw(9) << "n" << setw(9);
 	cout << "|" << setw(9) << "x" << setw(9);
-	cout << "|" << setw(9) << "ln(1-x)" << setw(9);
-	cout << "|" << setw(9) << "y" << setw(9)<< "|" << endl;
+	cout << "|" << setw(9) << "ln(1-x)" << setw(9); 
+	cout << "|" << setw(9) << "y" << setw(9);
+	cout << "|" << setw(9) << "n" << setw(9)<< "|" << endl;
 	cout << string(73, '-') << endl;
+
+	cout << fixed;
+	cout.precision(6);
+
 	x = x1;
+	
 	while (x<=x2)
 	{
-	  int i = 1;
-      while (i<n)
+		cout << "|" << setw(11) << x << setw(7) << "|";
+		cout << setw(11) << log(1 - x) << setw(7) << "|";
+		
+		int i = 0;
+		y = 0;
+
+      while (abs(y) <= eps)
 	  {
+		i++;
 		y +=(pow(x, i) / i);
-		if (abs(y) < eps)
-			break;
-		i++; 
+		
+		if (i > i_max) 
+		break;
+		
 	  }
-	  y = -1*y;
-	  p = log(1 - x);
-	  cout << "|" << setw(11) << i << setw(7) << "|";
-	  cout << setw(11) << x << setw(7) << "|";
-	  cout << setw(11) << p << setw(7) << "|";
-	  cout << setw(11) << y << setw(7) << "|"<< endl;
-	  x += dx;
+	   if (i > i_max)
+		  cout << setw(11) << "Excess limit" << setw(6) << "|";
+	   else 
+		  cout << setw(11) << -1 * y << setw(7) << "|";
+	   cout << setw(11) << i << setw(7) << "|" << endl;
+	   x += dx;
 	}
 	cout << string(73, '-') << endl;
 	return 0;
