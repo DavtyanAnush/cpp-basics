3af275db773987d472187a96ab2c4e6ffdfa8252
Anush <*dawtyan.anush@yandex.ru*>
2018-10-03 20:13:03 +0300

    New commit

--- 3-conditionals-loops-2/main.cpp
+++ 3-conditionals-loops-2/main.cpp
@@ -7,22 +7,22 @@ using namespace std;
 
 int main()
 {
-	double x, x1, x2, dx, y, sum, eps;
+	double x, x1, x2, dx, nth_term, ln, eps;
 	const double kEPS = 1e-15;
-	const int i_max=100;
+	int n, max_iter = 1000000;
 
 	check:
 	cout << "ln(1-x), -1 <= x < 1"<<endl<< "Enter x1, x2, dx, eps" << endl;
 	cin >> x1 >> x2 >> dx >> eps;
-	if (((fabs(dx) < kEPS)||((x1 >= 1)||(x1 < -1))||((x2 >= 1)||(x2 < -1))))
+	if ((dx < kEPS)||(eps < kEPS)||(x1 < -1)||(x1>=x2)||(x2 >= 1))
 	{
 		cout << "Input error" << endl;
 		goto check;
 	}
 	cout << string(73, '-') << endl; 
 	cout << "|" << setw(9) << "x" << setw(9);
-	cout << "|" << setw(9) << "ln(1-x)" << setw(9); 
-	cout << "|" << setw(9) << "y" << setw(9);
+	cout << "|" << setw(9) << "ln(1-x) built-in" << setw(2);
+	cout << "|" << setw(9) << "  ln(1-x) series ";
 	cout << "|" << setw(9) << "n" << setw(9)<< "|" << endl;
 	cout << string(73, '-') << endl;
 
@@ -35,30 +35,24 @@ int main()
 	{
 		cout << "|" << setw(11) << x << setw(7) << "|";
 		cout << setw(11) << log(1 - x) << setw(7) << "|";
+		ln = 0;
+		for (n = 1; n < max_iter; n++)
+		{
+			nth_term = -pow(x, n) / n;
+			ln += nth_term;
 
-		y = x;
-		int i = 1;
-		sum = -1*x;
-
-      while (fabs(y)> eps)
-	  {
-		i++;
-		y =-1*(pow(x, i) / i);
-		sum = sum + y;
-		
-		if (i > i_max) 
-		 break;
-	  }
-	   if (i > i_max) 
-	   {
-		  cout << setw(11) << "Excess limit" << setw(6) << "|";
-		  cout << setw(11) << " " << setw(7) << "|" << endl;
-	   }
-	   else 
-	   {
-		   cout << setw(11) << sum << setw(7) << "|";
-		   cout << setw(11) << i << setw(7) << "|" << endl;
-	   }
+			if (abs(nth_term) < eps)
+				break;
+		}
+		if (n == max_iter) {
+			cout << setw(11) << "Excess limit" << setw(6) << "|";
+			cout << setw(11) << " " << setw(7) << "|" << endl;
+		}
+		else 
+		{
+			cout << setw(11) << ln << setw(7) << "|";
+			cout << setw(11) << n << setw(7) << "|" << endl;
+		}
 	   x += dx;
 	}
 	cout << string(73, '-') << endl;
