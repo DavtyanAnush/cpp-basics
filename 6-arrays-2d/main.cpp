2f83532fba5ad6bc8cb1a513e3e55ecbfc0324a5
Anush <*dawtyan.anush@yandex.ru*>
2018-10-08 20:41:46 +0300

    2 commit

--- 6-arrays-2d/main.cpp
+++ 6-arrays-2d/main.cpp
@@ -1,30 +1,41 @@
 #include <iostream>
+#include <fstream>
+#include <iomanip>
+
 
 using namespace std;
 
-void computeSeddlePoint(int i, int j, int min, int  max, int array[4][4], int const kRow, int const kCol);
+void findSeddlePoint(int i, int j,
+	int min, int  max, int array[4][4], 
+	int const kRow, int const kCol);
 
 int main()
 {
 	int i, j, min, max, sum;
 	int const kRow = 4, kCol = 4;
 
-	int array[kRow][kCol] = {
-	{2, 4, 6, 2},
-	{1, 74, -6, -10},
-	{2, 4, 5, 2},
-	{-2,7, 2, -1}
-	};
+	int array[kRow][kCol];
+	ifstream fin("array.txt");
+	if (!fin.is_open()) 
+	{
+		cout << "can't open file" << endl;
+		return 1;
+	}
+	for (int i = 0; i < kRow; i++) 
+		for (int j = 0; j < kCol; j++) 
+		{
+			fin >> array[i][j];
+		}
 
-	//Вывод массива
+	
 	for (i = 0; i < kRow; i++) {
 		for (j = 0; j < kCol; j++)
 		{
-			cout << array[i][j] << "  ";
-		} cout << endl;
+			cout << setw(3) << array[i][j] << setw(2) << "|";
+		} 
+		cout << endl;
 	}
 
-	//Нахождение суммы элементов...
 	for (i = 0; i < kRow; i++) {
 		for (j = 0; j < kCol; j++)
 			if (array[i][j] < 0)
@@ -37,13 +48,13 @@ int main()
 			}
 	}
 
-	//Нахождение седловых точек
+	
 	for (j = 0; j < kCol; j++) {
 		for (i = 0; i < kRow; i++)
 		{
 			min = array[i][j];
 			max = min;
-			computeSeddlePoint(i, j, min, max, array, kRow, kCol);
+			findSeddlePoint(i, j, min, max, array, kRow, kCol);
 		} cout << endl;
 		break;
 	}
@@ -51,7 +62,8 @@ int main()
 	return 0;
 }
 
-void computeSeddlePoint(int i, int j, int min, int  max, int array[4][4], int const kRow, int const kCol) {
+void findSeddlePoint(int i, int j, int min, int  max, 
+	int array[4][4], int const kRow, int const kCol) {
 	int i_1 = i;
 	int j_1 = j;
 	int i_2 = i;
@@ -73,7 +85,9 @@ check:
 			i_2 = i;
 		}
 	if (i_1 == i_2)
-		cout << "Seddle point: " << max << "[" << i_1 << "]" << "[" << j_1 << "]" << endl;
+		cout << "Seddle point: " << max 
+		<< "[" << i_1 << "]" 
+		<< "[" << j_1 << "]" << endl;
 
 	i = i_1;
 	for (j = j_1 + 1; j < kCol; j++)
