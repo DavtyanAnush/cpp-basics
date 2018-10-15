68974e15dafda0c0feac39bc53f32d6f0fd12eef
Anush <*dawtyan.anush@yandex.ru*>
2018-10-15 17:42:10 +0300

    5 commit

--- 6-arrays-2d/main.cpp
+++ 6-arrays-2d/main.cpp
@@ -6,76 +6,76 @@ using namespace std;
 
 int main()
 {
-	int const kRow = 4, kCol = 4;
+	int const kRow = 4, kCol = 5;
 
 	int array[kRow][kCol];
 	ifstream fin("array.txt");
 	if (!fin.is_open())
 	{
-		cout << "can't open file" << endl;
+		cout << "can't open file array.txt " << endl;
 		return 1;
 	}
 
-	for (int i = 0; i < kRow; i++)
+	for (int k = 0; k < kRow; k++)
 	{
 		for (int j = 0; j < kCol; j++)
 		{
-			fin >> array[i][j];
-			cout << setw(3) << array[i][j] << setw(2) << "|";
+			fin >> array[k][j];
+			cout << setw(3) << array[k][j] << setw(2) << "|";
 		}
 	   cout << endl;
     }
 
-	for (int i = 0; i < kRow; i++) 
+	for (int k = 0; k < kRow; k++) 
 	{
 		for (int j = 0; j < kCol; j++)
-			if (array[i][j] < 0)
+			if (array[k][j] < 0)
 			{
 				int sum = 0;
 				for (j = 0; j < kCol; j++)
-					sum += array[i][j];
+					sum += array[k][j];
 				cout << "Sum=" << sum << endl;
 				break;
 			}
 	}
 	
-	for (int k = 0; k < kRow; k++)
+	for (int i = 0; i < kRow; i++)
 	{
-		int min = array[k][0];
-		int i_row_min = k;
-		int j_col_min = 0;
-		int i_row_max = k;
+		int row_min = array[i][0];
+		int row_min_i = i;
+		int row_min_j = 0;
+		
 
-		for (int j = 0; j < kCol; j++)
+		for (int j = 1; j < kCol; j++)
 		{
-			if (array[k][j] < min)
+			if (array[i][j] < row_min)
 			{
-				min = array[k][j];
-				i_row_min = k;
-				j_col_min = j;
+				row_min = array[i][j];
+				row_min_i = i;
+				row_min_j = j;
 			}
 		}
 	check:
-		int max = min;
-		for (int i = 0; i < kRow; i++)
-			if (array[i][j_col_min] > max)
+		int col_max = row_min;
+		int col_max_i = i;
+		for (int k = 0; k < kRow; k++)
+			if (array[k][row_min_j] > col_max)
 			{
-				max = array[i][j_col_min];
-				i_row_max = i;
+				col_max = array[k][row_min_j];
+				col_max_i = k;
 			}
 
-		if (i_row_min == i_row_max)
-			cout << "Saddle point: " << max
-			<< "[" << i_row_min << "]"
-			<< "[" << j_col_min << "]" << endl;
+		if (row_min_i == col_max_i)
+			cout << "Saddle point: " << col_max
+			<< "[" << row_min_i << "]"
+			<< "[" << row_min_j << "]" << endl;
 
-		for (int j = j_col_min + 1; j < kCol; j++)
+		for (int j = row_min_j + 1; j < kCol; j++)
 		{
-			if (array[i_row_min][j] == min)
+			if (array[row_min_i][j] == row_min)
 			{
-				min = array[i_row_min][j];
-				i_row_max = i_row_min;
-				j_col_min = j;
+				col_max_i = row_min_i;
+				row_min_j = j;
 
 				goto check;
 			}
