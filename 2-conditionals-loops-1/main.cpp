809d9f292c6b139017677a8c354e9490b4b53cec
Anush <*dawtyan.anush@yandex.ru*>
2018-10-15 17:05:32 +0300

    copy

--- 2-conditionals-loops-1/main.cpp
+++ 2-conditionals-loops-1/main.cpp
@@ -7,9 +7,9 @@ using namespace std;
 
 int main()
 {
-	double x1, x2, dx, a, b, c, f, x;
-	const double EPS = 1e-15;
+	const double kEps = 1e-15;
 
+	double a, b, c, x1, x2, dx;
 	cout << "Enter a: ";
 	cin >> a;
 	cout << "Enter b: ";
@@ -23,7 +23,7 @@ int main()
 	cout << "Enter dx: ";
 	cin >> dx;
 
-	if ((abs(dx) > EPS) && (abs(c) > EPS))
+	if (dx >= kEps)
 	{
 		cout << endl << string(37, '-') << endl;
 		cout << "|" << setw(9) << "x" << setw(9);
@@ -33,38 +33,45 @@ int main()
 		cout << fixed;
 		cout.precision(3);
 
-		x = x1;
+		double f, x = x1;
 		while (x <= x2)
 		{
-			if ((c < 0) && (abs(b) > EPS))
+			cout << "|" << setw(11) << x << setw(7) << "|";
+
+			if ((c < 0) && (abs(b) >= kEps))
 			{
 				f = a * pow(x, 2) + pow(b, 2) * x;
 			}
 			else
 			{
-				if ((c > 0) && (abs(b) < EPS))
+				if ((c > 0) && (abs(b) < kEps))
 				{
-					if (abs(x + c) > EPS)
+					if (abs(x + c) >= kEps)
 						f = (x + a) / (x + c);
 					else
 					{
-						cout << "|" << setw(11) << x << setw(7);
-						cout << "|" << "  division by 0  |\n";
+						cout << "  division by 0  |\n";
 						x += dx;
 						continue;
 					}
 				}
 				else
-					f = x / c;
+				{
+					if (abs(c) >= kEps)
+						f = x / c;
+					else
+					{
+						cout << "  division by 0  |\n";
+						x += dx;
+						continue;
+					}
+				}
 			}
-
-			cout << "|" << setw(11) << x << setw(7) << "|";
 
 			if (((int(a) & int(b)) | (int(a) & int(c))) != 0)
 				cout << setw(11) << f << setw(7);
 			else
 				cout << setw(9) << int(f) << setw(9);
-
 			cout << "|" << endl;
 
 			x += dx;
@@ -72,7 +79,7 @@ int main()
 		cout << string(37, '-') << endl;
 	}
 	else
-		cout << "\nError!\n";
+		cout << "\nError dx!\n";
 
 	return 0;
 }
